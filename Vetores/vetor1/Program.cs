453bfd2b7704191c2bb58fabe7c4cd813868e2ab
Guilherme Marques <guilhermemarquesf0@gmail.com>
2026-08-25 18:13:55 -0300

    first commit

---
+++ Vetores/vetor1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace vetores 
+{
+    class Program
+    {
+        static int main()
+        {
+           int Fat = 1;
+           int n;
+           
+           Console.WriteLine("n = ");
+           n = int.Parse(Console.ReadLine()!);
+
+           Fat = CalcFat(n);
+
+           Console.WriteLine($"Fatorial = {fat}");
+
+           Console.ReadLine();
+
+           static int CalcFat(int x)
+           {
+                int Result = 1;
+
+                for (int i = 1; i <= x; i++)
+                    result = 1;
+
+                return Result;    
+           }
+
+
+
+        }                                             
+    }
+}
