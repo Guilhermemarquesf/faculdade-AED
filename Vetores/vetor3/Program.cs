f0149b215a02f603292b189d28a2fcd70ae18dc0
Guilherme Marques <guilhermemarquesf0@gmail.com>
2026-08-27 20:09:44 -0300

    first commit

---
+++ Vetores/vetor3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace vetor3
+{
+    class Program
+    {
+        static void Main()
+        {
+           int x, y, z;
+
+           (x, y, z) = MyFunção();
+
+           Console.WriteLine($"Os elementos sao {x} - {y} - {z}");
+
+           Console.ReadKey();
+
+           x = MyFunção().Item1;
+
+           Console.WriteLine($"O primeiro elemento é: {x}");
+
+           Console.ReadKey();
+        }
+
+        static (int, int, int) MyFunção()
+        {
+            return (10, 20, 30);
+        }                                            
+    }
+}
