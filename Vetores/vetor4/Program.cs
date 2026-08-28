b177b863a32db8bab94b19ea11037c06a8150143
Guilherme Marques <guilhermemarquesf0@gmail.com>
2026-08-28 19:48:48 -0300

    first commit

---
+++ Vetores/vetor4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace vetor4
+{
+    class Program
+    {
+        static void Main()
+        {
+           int n;
+           int resultado;
+
+           Console.WriteLine("Digite um numero:");
+           n = int.Parse(Console.ReadLine()!);
+
+           resultado = Dobro(n);
+
+           Console.WriteLine($"Dobro {resultado}");
+
+           Console.ReadKey(); 
+           
+        }                                     
+
+        static int Dobro(int x)
+        {
+            return x * 2;
+        }        
+    }
+}
