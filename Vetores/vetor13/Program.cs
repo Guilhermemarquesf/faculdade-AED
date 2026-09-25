042a1344f622bc35bd70fc68775a08e42bdeea5d
Guilherme Marques <guilhermemarquesf0@gmail.com>
2026-09-24 21:44:31 -0300

    first commit

---
+++ Vetores/vetor13/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace vetor13
+{
+    class Program
+    {
+        static void Main()
+        {
+           long n;
+           long nTermo;
+
+            Console.WriteLine("Digite o termo da serie: ");
+            n = long.Parse(Console.ReadLine()!);
+
+            nTermo = CalcFibonacci(n);
+
+            Console.WriteLine($"\n O termo {n} da Série de Fibonacci é {nTermo}");
+
+            Console.ReadKey();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"{i,3} = {CalcFibonacciR(i)}");
+            }
+
+            Console.ReadKey();
+        }                  
+
+        static long CalcFibonacciR(long n)
+        {
+            if (n < 2)
+                return n;
+            else
+                return CalcFibonacciR(n - 1) + CalcFibonacciR(n - 2);
+        }   
+
+        static long CalcFibonacci(long n)
+        {
+            long atual = 0;
+            long anterior = 1;
+
+            for (long i = 0; i < n; i++)
+            {
+                long proximo = atual + anterior;
+                atual = anterior;
+                anterior = proximo;
+            }
+
+            return atual;
+        }                        
+    }
+}
